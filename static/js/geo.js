2171c27ef91d21b53c2bbf0b23543a6de24bddb9
Daniel Heath <daniel.r.heath@gmail.com>
2014-07-13 12:10:55 +1000

    Disable ‘near_me’ unless the browser offers location data

--- static/js/geo.js
+++ static/js/geo.js
@@ -1,10 +1,10 @@
-
 function onLocationAvailable(position) {
   var latitude = position.coords.latitude;
   var longitude = position.coords.longitude;
   $(function() {
     window.latlng = [latitude, longitude]
-    $("#location").val("Near me")
+    $("#location [value='near_me']").attr('disabled', false)
+    $("#location").val("near_me")
   })
 }
 
